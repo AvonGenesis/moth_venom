a426197927af546115a2bb5b33659c378c7141e0
japos003 <jrcapostol@gmail.com>
2016-10-22 09:30:53 -0700

    WASD Controls
    Added WASD control to the player.js file. Now, the player can control
    the ship with the standardized buttons.

--- scripts/systems/player.js
+++ scripts/systems/player.js
@@ -2,9 +2,18 @@ var left = keyboard(37),
   up = keyboard(38),
   right = keyboard(39),
   down = keyboard(40);
+  left2 = keyboard(65); //a
+  up2 = keyboard(87); //w
+  right2 = keyboard(68); //d
+  down2 = keyboard(83); //s
 
 //Left arrow key `press` method
 left.press = function() {
+  //Change the explorer's velocity when the key is pressed
+  spriteData.vx = -ECS.player.components.player.velocity;
+};
+
+left2.press = function() {
   //Change the explorer's velocity when the key is pressed
   spriteData.vx = -ECS.player.components.player.velocity;
 };
@@ -19,12 +28,30 @@ left.release = function() {
   }
 };
 
+left2.release = function() {
+  //If the left arrow has been released, and the right arrow isn't down,
+  //and the spriteData isn't moving vertically:
+  //Stop the spriteData
+  if (!right2.isDown) {
+    spriteData.vx = 0;
+  }
+};
+
 //Up
 up.press = function() {
   spriteData.vy = -ECS.player.components.player.velocity;
 };
 up.release = function() {
   if (!down.isDown) {
+    spriteData.vy = 0;
+  }
+};
+
+up2.press = function() {
+  spriteData.vy = -ECS.player.components.player.velocity;
+};
+up2.release = function() {
+  if (!down2.isDown) {
     spriteData.vy = 0;
   }
 };
@@ -39,12 +66,30 @@ right.release = function() {
   }
 };
 
+right2.press = function() {
+  spriteData.vx = ECS.player.components.player.velocity;
+};
+right2.release = function() {
+  if (!left2.isDown) {
+    spriteData.vx = 0;
+  }
+};
+
 //Down
 down.press = function() {
   spriteData.vy = ECS.player.components.player.velocity;
 };
 down.release = function() {
   if (!up.isDown) {
+    spriteData.vy = 0;
+  }
+};
+
+down2.press = function() {
+  spriteData.vy = ECS.player.components.player.velocity;
+};
+down2.release = function() {
+  if (!up2.isDown) {
     spriteData.vy = 0;
   }
 };
